f4d7ffd2c2ab39bebb56160a1af57eaa17cc3526
Antonio Blandin <antonio.blandin25@gmail.com>
2019-11-18 18:08:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,19 +1,19 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
-
+  array.last
 def using_last(array)
 
 end
